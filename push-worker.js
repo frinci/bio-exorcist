f7c3528e6c65429c443b0590470152b0ac27ee7d
Frenchy French <frinci@frenchys-mbp.home>
2021-01-07 00:05:34 -0500

    added alert prompt with message and title

--- push-worker.js
+++ push-worker.js
@@ -6,18 +6,5 @@ uaSetup.worker(self, {
   defaultActionURL: 'https://bio\u002Dexorcist.netlify.app/',
   appKey: 'omzMcysgTXe5ihgOxHWwbw',
   token: 'MTpvbXpNY3lzZ1RYZTVpaGdPeEhXd2J3Om5WUkQxcEJNcW5ac3piT1VvMnhYV0ZEdU5qNlF2Nmx5SVNmQzlvWS1HREk',
-  vapidPublicKey: 'BMbeJ1ehcdb-GsYBIB32YtAFKPxa9CNZIQDdLmZhH0kchB5PN_6kr61yGI9E3VnhLYG_Xpy_bpJ3p-57BnCu1qk=',
-  workerUrl: '/push-worker.js'
+  vapidPublicKey: 'BMbeJ1ehcdb-GsYBIB32YtAFKPxa9CNZIQDdLmZhH0kchB5PN_6kr61yGI9E3VnhLYG_Xpy_bpJ3p-57BnCu1qk='
 })
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/push-worker.js').then(function(registration) {
-        // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-  });
-}
